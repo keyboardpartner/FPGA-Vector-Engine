956d5007b3a9d8e126412c90a2b4e0dc4e716927
HX3 Development Team <info@keyboardpartner.de>
2024-10-31 13:05:04 +0100

    Add files via upload

---
+++ sdcard_644/copy_ast_bitfile.bat
@@ -0,0 +1,1 @@
+copy "E:\Dropbox\AsteroidsClock\FPGA ctlab-asteroids\asteroidsclockavr.bit" ast_clk.bit
---
+++ sdcard_644/copy_logos_here.bat
@@ -0,0 +1,15 @@
+REM copy "E:\Dropbox\AsteroidsClock\FPGA ctlab-asteroids\asteroidsclockavr.bit" ast_clk.bit
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_hammond.VEC" LOGO_0.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_harley.VEC" LOGO_1.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_hx3.VEC" LOGO_2.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_kuba.VEC" LOGO_3.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_kbp.VEC" LOGO_4.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_labor.VEC" LOGO_5.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_makey.VEC" LOGO_6.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_ti.VEC" LOGO_7.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_nasa.VEC" LOGO_8.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_ufo.VEC" LOGO_9.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_spartan.VEC" LOGO_10.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_hx3.VEC" LOGO_11.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\LOGO_tek59.VEC" LOGO_12.VEC
+copy "E:\Dropbox\AsteroidsClock\Logos\SPLASH.VEC" SPLASH.VEC
